45766e3071900d3cc8b13f7525790c5b6c1e956a
anzharip <anzhari.p@gmail.com>
2015-07-16 13:30:09 +0700

    Deleting unused files

--- serialize.py
+++
@@ -1,6 +0,0 @@
-def serialize(user):
-    username = user.username
-    profile_picture = user.profile_picture
-    full_name = user.full_name
-    id_ = user.id
-    return {'username': username, 'profile_picture': profile_picture, 'full_name': full_name, 'id': id_}
